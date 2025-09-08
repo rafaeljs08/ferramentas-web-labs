a18b83200914e2e0066902fc90efc183ea2bc3c8
Vitorfos <vitorfosanglo@gmail.com>
2025-09-08 17:30:56 -0300

    Aqruivos Iniciais do projeto

---
+++ src/test/java/br/fepi/si/parcelamentos/ParcelamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fepi.si.parcelamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcelamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
