7191bcc1994ec736a2ff7b3f66a0a6887445013c
rafaeljs08 <rafaeljs08@hotmail.com>
2025-11-11 18:11:24 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/br/fepi/si/parcelamentos/ParcelamentosApplication.java
@@ -0,0 +1,13 @@
+package br.fepi.si.parcelamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcelamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcelamentosApplication.class, args);
+	}
+
+}
